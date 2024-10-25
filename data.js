0e643cce20423a223fee1548ca4f184f367c57d3
Marco Tulio Costa <marcotscosta@gmail.com>
2024-10-25 15:46:01 -0300

    Update data.js

--- data.js
+++ data.js
@@ -1,58 +1,59 @@
 const shoppingData = [
   {
     "Café da Manhã": [
-      { "itemId": 1, "name": "Ovos", "checked": false },
-      { "itemId": 2, "name": "Pão de forma", "checked": false },
-      { "itemId": 3, "name": "Frios (presunto, queijo, etc.)", "checked": false },
-      { "itemId": 4, "name": "Requeijão", "checked": false },
-      { "itemId": 5, "name": "Pó de café", "checked": false },
-      { "itemId": 6, "name": "Leite", "checked": false }
+      { "itemId": 1.1, "name": "Ovos", "checked": false },
+      { "itemId": 1.2, "name": "Pão de forma", "checked": false },
+      { "itemId": 1.3, "name": "Frios (presunto, queijo, etc.)", "checked": false },
+      { "itemId": 1.4, "name": "Requeijão", "checked": false },
+      { "itemId": 1.5, "name": "Pó de café", "checked": false },
+      { "itemId": 1.6, "name": "Leite", "checked": false },
+      { "itemId": 1.7, "name": "Bolinho de formigueiro", "checked": false }
     ]
   },
   {
     "Lanches": [
-      { "itemId": 7, "name": "Frango desfiado pré-pronto", "checked": false },
-      { "itemId": 8, "name": "Frios (presunto, queijo, etc.)", "checked": false }
+      { "itemId": 2.1, "name": "Frango desfiado pré-pronto", "checked": false },
+      { "itemId": 2.2, "name": "Frios (presunto, queijo, etc.)", "checked": false }
     ]
   },
   {
     "Jantares": {
       "Jantar 1": [
-        { "itemId": 9, "name": "Macarrão tipo ninho", "checked": false },
-        { "itemId": 10, "name": "Bacon em cubos", "checked": false },
-        { "itemId": 11, "name": "Calabresa", "checked": false },
-        { "itemId": 12, "name": "Creme de leite fresco", "checked": false },
-        { "itemId": 13, "name": "Queijo parmesão ralado", "checked": false },
-        { "itemId": 14, "name": "Manteiga", "checked": false },
-        { "itemId": 15, "name": "Azeite", "checked": false }
+        { "itemId": 3.1, "name": "Macarrão tipo ninho", "checked": false },
+        { "itemId": 3.2, "name": "Bacon em cubos", "checked": false },
+        { "itemId": 3.3, "name": "Calabresa", "checked": false },
+        { "itemId": 3.4, "name": "Creme de leite fresco", "checked": false },
+        { "itemId": 3.5, "name": "Queijo parmesão ralado", "checked": false },
+        { "itemId": 3.6, "name": "Manteiga", "checked": false },
+        { "itemId": 3.7, "name": "Azeite", "checked": false }
       ],
       "Jantar 2": [
-        { "itemId": 16, "name": "Bife grosso (contra-filé, ancho ou ribeye)", "checked": false },
-        { "itemId": 17, "name": "Arroz", "checked": false },
-        { "itemId": 18, "name": "Caldo de carne", "checked": false },
-        { "itemId": 19, "name": "Vinho tinto", "checked": false }
+        { "itemId": 4.1, "name": "Bife grosso (contra-filé, ancho ou ribeye)", "checked": false },
+        { "itemId": 4.2, "name": "Arroz", "checked": false },
+        { "itemId": 4.3, "name": "Caldo de carne", "checked": false },
+        { "itemId": 4.4, "name": "Vinho tinto", "checked": false }
       ]
     }
   },
   {
     "Temperos e Especiarias": [
-      { "itemId": 20, "name": "Sal e pimenta-do-reino", "checked": false },
-      { "itemId": 21, "name": "Noz-moscada", "checked": false },
-      { "itemId": 22, "name": "Salsinha picada (opcional)", "checked": false },
-      { "itemId": 23, "name": "Alho amassado (2 dentes)", "checked": false },
-      { "itemId": 24, "name": "Tomilho ou alecrim (opcional)", "checked": false },
-      { "itemId": 25, "name": "Cebolinha ou salsinha picada (para finalizar)", "checked": false },
-      { "itemId": 26, "name": "Cebola pequena picada (para o arroz)", "checked": false },
-      { "itemId": 27, "name": "Alho picado (2 dentes, para o arroz)", "checked": false }
+      { "itemId": 5.1, "name": "Sal e pimenta-do-reino", "checked": false },
+      { "itemId": 5.2, "name": "Noz-moscada", "checked": false },
+      { "itemId": 5.3, "name": "Salsinha picada (opcional)", "checked": false },
+      { "itemId": 5.4, "name": "Alho amassado (2 dentes)", "checked": false },
+      { "itemId": 5.5, "name": "Tomilho ou alecrim (opcional)", "checked": false },
+      { "itemId": 5.6, "name": "Cebolinha ou salsinha picada (para finalizar)", "checked": false },
+      { "itemId": 5.7, "name": "Cebola pequena picada (para o arroz)", "checked": false },
+      { "itemId": 5.8, "name": "Alho picado (2 dentes, para o arroz)", "checked": false }
     ]
   },
   {
     "Verduras e Legumes": [
-      { "itemId": 28, "name": "Tomates", "checked": false },
-      { "itemId": 29, "name": "Cenoura", "checked": false },
-      { "itemId": 30, "name": "Cebola", "checked": false },
-      { "itemId": 31, "name": "Alho", "checked": false },
-      { "itemId": 32, "name": "Limão", "checked": false }
+      { "itemId": 6.1, "name": "Tomates", "checked": false },
+      { "itemId": 6.2, "name": "Cenoura", "checked": false },
+      { "itemId": 6.3, "name": "Cebola", "checked": false },
+      { "itemId": 6.4, "name": "Alho", "checked": false },
+      { "itemId": 6.5, "name": "Limão", "checked": false }
     ]
   }
 ]
